124449953786e9c0fe2f4614e85624eafc5f6119
Sunil kathait <5890851+allaboutopensource@users.noreply.github.com>
2020-07-23 14:35:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  default = "administrator@company.com"
+}
+
+variable "vsphere_password" {
+  default = "secret-password"
+}
+
+variable "vsphere_server" {
+  default = "vcenter-server-FQDN"
+}
